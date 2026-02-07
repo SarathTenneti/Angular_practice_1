738f0dbda75eb569e8da463f96342b187389fc10
unknown <saratht.tenneti@gmail.com>
2026-02-07 19:20:12 +0530

    getting 10% discount of every product

--- src/app/rxjs-basics/rxjs-basics.ts
+++ src/app/rxjs-basics/rxjs-basics.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { from, of, map,switchMap, concatMap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
@@ -10,51 +11,20 @@ import { Observable } from 'rxjs/internal/Observable';
 })
 export class RxjsBasics {
 
-  cityList : string[] = ["hyd","delhi","chennai"];
-  cityList$ = of(["hyd","delhi","chennai"]);
-  cityList2$ = from(["hyd","1"]);
-  
-  source$ = of( {id : 1 , name :"sarath"},
-                {id : 2 , name :"harish"}
-  );
-  answer$  = new Observable<number> ;
 
-  constructor()
+  constructor(private http : HttpClient)
   {
-    this.cityList$.subscribe( (cityData: string [] )=>{ 
-      debugger;
-      console.log(cityData) 
-    })
-    this.cityList2$.subscribe(  (cityData : string) =>{
-      debugger;
-      console.log(cityData)
-    })
-    const myObs$ =  new Observable(value=>{
-      value.next("This is a demo text");
-    })
-    myObs$.subscribe((message) =>{
-      debugger;
-      console.log(message);
-    });
     
-    //map - operator
-    
-    this.source$.pipe(
-      map(v => v.name)
-    ).subscribe( a => console.log(a));
-
-    // switch Map -operator
-
-    of(1,2,3).pipe(
-      switchMap( id => of(`User ${id}`))
+    this.http.get<any[]>("https://fakestoreapi.com/products").pipe(
+      map( products => 
+        products.map( p => ({
+          "discounted price" : p.price * 0.9,
+          "original price"  : p.price,
+          "discounted amount ": p.price *0.1
+        }))
+      )
     ).subscribe(console.log)
 
-    // conat Map - operator
-    
-     this.answer$ = of(1,2,3).pipe(
-      concatMap( x => of(x*10))
-    );
-    this.answer$.subscribe(console.log)
   }
 
 }
