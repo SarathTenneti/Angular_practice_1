0e2bf98613ddaed6d4c2e578ecbc6afe7bdee0fd
unknown <saratht.tenneti@gmail.com>
2026-02-07 21:46:49 +0530

    Search text logic code has minor errors

--- src/app/rxjs-basics/rxjs-basics.ts
+++ src/app/rxjs-basics/rxjs-basics.ts
@@ -1,30 +1,37 @@
 import { Component } from '@angular/core';
-import { from, of, map,switchMap, concatMap } from 'rxjs';
+import { from, of, map,switchMap, debounceTime,concatMap, distinctUntilChanged, filter, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-rxjs-basics',
-  imports: [],
+  imports: [FormsModule],     
   templateUrl: './rxjs-basics.html',
   styleUrl: './rxjs-basics.css',
 })
 export class RxjsBasics {
 
+  searchText$ = new Subject<string>() ;
+  searchTexts : string='';
 
   constructor(private http : HttpClient)
-  {
-    
-    this.http.get<any[]>("https://fakestoreapi.com/carts").pipe(
-      map(carts => 
-        carts.map(cart => 
-          cart.products.filter( (product : any) => 
-            (product.productId % 2 === 0)
-        ))
-      )
-    ).subscribe(console.log)
+  { 
+    this.searchText$ = this.searchTexts;
+   this.searches(this.searchText$ , http)
+  }
 
-    
+  searches(text : Observable<string> , http : HttpClient)
+  {
+    text.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      filter(text => text.length >0),
+      switchMap( text => 
+        text
+      )
+    ).subscribe(console.log);
+
   }
 
 }
